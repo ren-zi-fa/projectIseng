2a5fab5d108c77dc9730bc74ec65f54dc670b316
ren-zi-fa <renzifebriandika923@gmail.com>
2024-07-27 22:06:19 +0700

    initial commit

---
+++ slice.js
@@ -0,0 +1,26 @@
+import * as toolkit from "@reduxjs/toolkit";
+
+const { configureStore, createSlice } = toolkit;
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {
+    addToCart(state, action) {
+      state.push(action.payload);
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    cart: cartSlice.reducer,
+  },
+});
+
+console.log("oncreate store : ", store.getState());
+store.subscribe(() => {
+  console.log("store change: ", store.getState());
+});
+
+store.dispatch(cartSlice.actions.addToCart({ id: 1, qty: 20 }));
